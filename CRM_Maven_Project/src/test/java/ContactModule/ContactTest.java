92cc75cda47f814423454c52461ca87fb3c9a085
admin <admin@Dell>
2021-10-19 13:27:12 +0530

    First commit of maven-project study project

---
+++ CRM_Maven_Project/src/test/java/ContactModule/ContactTest.java
@@ -0,0 +1,17 @@
+package ContactModule;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	public void createContact() {
+		System.out.println("Contact has been created");
+	}
+
+	
+	@Test
+	public void searchContact() {
+		System.out.println("Contact has been searched");
+	}
+}
---
+++ CRM_Maven_Project/src/test/java/OrgModule/OrgTest.java
@@ -0,0 +1,15 @@
+package OrgModule;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void createOrg() {
+		System.out.println("Organisatiom has been created");
+	}
+
+	@Test
+	public void searchOrg() {
+		System.out.println("Organisation has been searched");
+	}
+}
